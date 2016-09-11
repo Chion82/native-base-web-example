c24f2c149c8370abfe66b9e4c011a1f513483f75
Chion Tang <sdspeedonion@gmail.com>
2016-09-11 17:27:07 +0800

    dist

---
+++ web/webpack.config.prod.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const DIRECTORY = path.join(__dirname)
+
+module.exports = {
+  //devtool : 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'src')
+  },
+  entry: [
+    'babel-polyfill',
+    'whatwg-fetch',
+    path.join(__dirname, '../index.web.js')
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: { cacheDirectory: true }
+      },
+      {
+        test: /\.(gif|jpe?g|png|svg)$/,
+        loader: 'url-loader',
+        query: { name: '[name].[hash:16].[ext]' }
+      }
+    ]
+  },
+  output: {
+    filename: './dist/bundle.js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web-extended',
+      'native-base': 'native-base-web',
+      //Icon.TabBarItem
+      'react-native-vector-icons/Ionicons': 'native-base-web/dist/Components/Widgets/Icon'
+    }
+  }
+}
